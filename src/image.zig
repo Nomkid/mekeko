f28d1ede9ae0e3504a69d95c77455b6554814840
Jeeves <gabriel.d.keess@outlook.com>
2023-05-25 01:21:11 -0600

    Bresenham's algorithm

--- src/image.zig
+++ src/image.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const t = @import("./types.zig");
 
 height: u32,
 width: u32,
@@ -64,4 +65,37 @@ test "draw vertical line" {
     while (y < size) : (y += 1) img.setPixel(0, y, [3]u8{ 255, 255, 255 });
 
     try img.writeToFile("./test/vline.ppm");
+}
+
+test "draw diagonal line" {
+    const size = 15;
+    var img = try create(size, size, std.testing.allocator);
+    defer img.deinit();
+
+    const start = t.Vector2{ .x = 1, .y = 1 };
+    const end = t.Vector2{ .x = 11, .y = 5 };
+
+    // Bresenham's algorithm, from https://gist.github.com/bert/1085538#file-plot_line-c
+    var x: i32 = 0;
+    var y: i32 = 0;
+    var dx: i32 = try std.math.absInt(@floatToInt(i32, end.x - start.x));
+    var sx: i32 = if (start.x < end.x) 1 else -1;
+    var dy: i32 = -try std.math.absInt(@floatToInt(i32, end.y - start.y));
+    var sy: i32 = if (start.y < end.y) 1 else -1;
+    var err: i32 = dx + dy;
+    var e2: i32 = undefined;
+    while (x < end.x and y < end.y) {
+        img.setPixel(@intCast(u32, x), @intCast(u32, y), [3]u8{ 255, 255, 255 });
+        e2 = 2 * err;
+        if (e2 >= dy) {
+            err += dy;
+            x += sx;
+        }
+        if (e2 <= dx) {
+            err += dx;
+            y += sy;
+        }
+    }
+
+    try img.writeToFile("./test/dline.ppm");
 }
